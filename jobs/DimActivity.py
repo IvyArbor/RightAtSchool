260b8b0f0e619f5dbf963d5cbf9b75eb8861ebab
nuhi <nuhibesimi@gmail.com>
2017-11-07 11:22:47 +0100

    DimActivity Integrate Time Dimension

--- jobs/DimActivity.py
+++ jobs/DimActivity.py
@@ -105,9 +105,9 @@ class DimActivity(CSVJob):
             ]
 
             row["Activity Number"] = int(row["Activity Number"])
-            row["End Date"] = parser.parse(row["End Date"])
-            row["Start Date"] = parser.parse(row["Start Date"])
-            row["Transaction Date"] = parser.parse(row["Transaction Date"])
+            row["End Date"] = self.getTimeId(cursor, row["End Date"])
+            row["Start Date"] = self.getTimeId(cursor, row["Start Date"])
+            row["Transaction Date"] = self.getTimeId(cursor, row["Transaction Date"])
 
             name_placeholders = ", ".join(["`{}`".format(s) for s in databasefieldvalues])
             print(name_placeholders)
@@ -122,15 +122,59 @@ class DimActivity(CSVJob):
         """Here we should archive the file instead"""
         # self.active_cursor.close()
 
+    def getTimeId(self, cursor, value):
+        query = """
+            SELECT `TimeId`
+            FROM `DimTime`
+            WHERE `Year` = %s AND `DayOfYear` = %s AND `Quarter` = %s AND `Month` = %s
+            AND `MonthName` = %s AND `DayOfMonth` = %s AND `Week` = %s AND `DayOfWeek` = %s
+            AND `CalendarDate` = %s AND `DateTimeStamp` = %s
+            ORDER BY `TimeId` DESC
+            LIMIT 1
+        """
+
+        result = self.parseTime(value)
+        cursor.execute(query, (result["Year"], result["DayOfYear"], result["Quarter"], result["Month"], result["MonthName"],
+                  result["DayOfMonth"], result["Week"], result["DayOfWeek"], result["CalendarDate"],
+                  result["DateTimeStamp"]))
+
+        query_result = cursor.fetchone()
+        if query_result == None:
+            return self.insertTime(cursor, value)
+        else:
+            return query_result[0]
+
+    def insertTime(self, cursor, value):
+        databasefieldvalues = ["Year", 'DayOfYear', 'Quarter', 'Month', 'MonthName', 'DayOfMonth', 'Week', 'DayOfWeek', 'CalendarDate', 'DateTimeStamp']
+
+        result = self.parseTime(value)
+        values = [result["Year"], result["DayOfYear"], result["Quarter"], result["Month"], result["MonthName"], result["DayOfMonth"], result["Week"], result["DayOfWeek"], result["CalendarDate"], result["DateTimeStamp"]]
+
+        name_placeholders = ", ".join(["`{}`".format(s) for s in databasefieldvalues])
+        value_placeholders = ", ".join(['%s'] * len(values))
+        sql = "INSERT INTO `{}` ({}) VALUES ({}) ".format("DimTime", name_placeholders, value_placeholders)
+
+        cursor.execute(sql, tuple(values))
+        self.target_connection.commit()
+        inserted_id = cursor.lastrowid
+
+        return inserted_id
+
+
     def parseTime(self, dt):
         date = parser.parse(dt)
 
         result = {}
         result["Year"] = date.year
+        result["DayOfYear"] = date.timetuple().tm_yday
         result["Quarter"] = int(math.ceil(date.month / 3.))
         result["Month"] = date.month
+        result["MonthName"] = date.strftime('%B')
+        result["DayOfMonth"] = date.timetuple().tm_mday
         result["Week"] = date.isocalendar()[1]
-        result["Day"] = date.day
+        # DayOfWeek can have values from 1 to 7 (1 for Monday - 7 for Sunday)
         result["DayOfWeek"] = date.weekday() + 1
-
+        result["CalendarDate"] = date
+        # I don't like this part
+        result["DateTimeStamp"] = (date - datetime(1970, 1, 1)).total_seconds()
         return result
