e1447f08620a86a1e86143cc8ed32ad544de362c
nuhi <nuhibesimi@gmail.com>
2017-12-28 11:45:15 +0100

    Settings - Production Version

--- settings.py
+++ settings.py
@@ -45,6 +45,7 @@ conf = {
             "ssl": None if env.get('STG_USE_SSL') == 'No' else {
                 'ca': env.get('STG_SSL_CA_BUNDLE'),
             },
+        },
         "rightatschool_productiondb": {
             "host": env.get('STG_HOST'),
             "user": env.get('STG_USER'),
@@ -53,6 +54,7 @@ conf = {
             "ssl": None if env.get('STG_USE_SSL') == 'No' else {
                 'ca': env.get('STG_SSL_CA_BUNDLE'),
             },
+        },
         "DW": {
             "host": env.get('DW_HOST'),
             "user": env.get('DW_USER'),
